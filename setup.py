d0f4ad968d2e4312ab6faf996848ad7a3ee6931e
Brian Stinson <bstinson@ksu.edu>
2014-06-13 23:43:32 -0500

    add in setup.py and default centpkg.conf

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "centpkg",
+    version = "0.0.1",
+    author = "Brian Stinson",
+    author_email = "bstinson@ksu.edu",
+    description = "CentOS Plugin to rpkg for managing RPM package sources",
+    url = "http://bitbucket.org/bstinsonmhk/centpkg.git",
+    license = "GPLv2+",
+    package_dir = {'': 'src'},
+    packages = ['centpkg'],
+    scripts = ['src/bin/centpkg'],
+    data_files = [('/etc/rpkg',['src/centpkg.conf']),]
+)
